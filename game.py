f79fddba4e3dcc83e66b86bede6898a8a81a25d8
carson <carsin@users.noreply.github.com>
2017-08-17 12:53:36 -1000

    Update to python 3

--- game.py
+++ game.py
@@ -1,14 +1,13 @@
+global gameMap
+global currTurn
+global changeTurn
+global turnNum
+
 def main():
-  global gameMap
-  global currTurn
-  global changeTurn
-  global turnNum
-  
   gameMap = [["_" for i in range(3)] for j in range(3)]
   currTurn = "X"
   changeTurn = True
   turnNum = 0
-  
   # main game loop
   playing = True
   while (playing):
@@ -17,8 +16,8 @@ def main():
     print("|      Turn #" + str(turnNum) + "      |")
     print("|  Player " + currTurn + "'s turn  |")
     print("-===================-")
-    printMap()
-    getUserInput()
+    printMap(gameMap)
+    getUserInput(gameMap, currTurn)
     # TODO: Fix turn changing after first
     if (changeTurn == True):
       if (currTurn == "X"):
@@ -31,22 +30,22 @@ def main():
       print("")
 
 # print game map
-def printMap():
+def printMap(map):
   print("|---- 1 - 2 - 3 ----|")
   for y in range(3):
-    print("| " + str(y + 1)),
+    print("| " + str(y + 1), end="")
     for x in range(3):
       if (x == 0):
-        print("| " + gameMap[x][y] + " |"),
+        print(" | " + map[x][y] + " | ", end="")
       elif (x == 2):
-        print("" + gameMap[x][y] + " |   |"),
+        print(" " + map[x][y] + " |   |", end="")
       else: 
-        print(gameMap[x][y] + " |"),
-    print("\n|    -----------    |")
+        print(map[x][y] + " |", end="")
+    print("\n|    -----------    |",)
   print("-===================-")
 
 # get user input of column and row
-def getUserInput():
+def getUserInput(map, currTurn):
   try:
     colChoice = int(input("Enter a COLUMN: "))
     if (colChoice > 3 or colChoice < 1):
@@ -58,11 +57,11 @@ def getUserInput():
           raise ValueError("Error: Row choice MUST be between 1 and 3")
         else:
           try:
-            if (gameMap[colChoice - 1][rowChoice - 1] == "_"):
-              gameMap[colChoice - 1][rowChoice - 1] = currTurn
+            if (map[colChoice - 1][rowChoice - 1] == "_"):
+              map[colChoice - 1][rowChoice - 1] = currTurn
               changeTurn = True
             else:
-              raise ValueError("Error: Position already taken by player " + gameMap[colChoice - 1][rowChoice - 1])
+              raise ValueError("Error: Position already taken by player " + map[colChoice - 1][rowChoice - 1])
           except ValueError as err:
             print(err)
             changeTurn = False
