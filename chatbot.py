eb8ee517c16262ee143bcb8afd73f1ae49cb462c
carson <carsin@users.noreply.github.com>
2017-09-01 12:18:59 -1000

    chatbot: v1

--- chatbot.py
+++ chatbot.py
@@ -1,58 +1,88 @@
 import random
 
-running = False
+name = "Unknown"
+age = 0
 
 def getName():
-    return input("What is your name? ")
+    name = input("What is your name? ")
+    print("Nice to meet you, " + name)
+    getAge(name)
 
-def getAge():
-    return int(input("How old are you? "))
+def getAge(name):
+    age = int(input("How old are you? "))
+    print("Ah, so you're " + str(age) + ".")
+    getMood(name, age)
 
-def getMood(name):
-    mood = input("How are you? ")
+def getMood(name, age):
+    mood = str.lower(input("How are you? "))
     if (mood == "bad" or mood == "not good" or mood == "awful" or mood == "horrible"):
         getRandomResponse("bad")
         print("I'll leave you alone. Goodbye, feel better, " + name + ".")
-        running = False
     else:
         getRandomResponse("good")
+        randNum = random.randint(0, 1)
+        if (randNum == 0):
+            getJob(name, age)
+        elif (randNum == 1):
+            getMarriage(name)
+        else:
+            print("Bad random number. Stopping.")
 
-def getJob():
-    return input("What do you do for a living? ")
+def getJob(name, age):
+    job = str(input("So, " + name + " what does a man of " + str(age) + " years old do for a living? "))
+    if (str.lower(job) == "nothing" or str.lower(job) == "unemployed" or str.lower(job) == "im not working" or str.lower(job) == "i'm not working" or str.lower(job) == "i'm unemployed" or str.lower(job) == "im unemployed"):
+        getRandomResponse("bad")
+    elif (str.lower(job) == "college" or str.lower(job) == "i go to college" or str.lower(job) == "school" or str.lower(job) == "i go to school" or str.lower(job) == "i'm in school" or str.lower(job) == "i go to university"):
+        getRandomResponse("good")
+        getSchooling(name, age)
+    else:
+        print("Interesting. " + job + " is a good job for you, " + name + ".")
+        getMarriage(name)
 
-def getMarriage():
-    return input("Are you married? ")
+def getMarriage(name):
+    marriage = input("Are you married? ")
+    if (str.lower(marriage) == "no" or str.lower(marriage) == "not yet" or str.lower(marriage) == "nope"):
+        getRandomResponse("bad")
+        goodbye(name)
+    else:
+        getRandomResponse("good")
+        goodbye(name)
+
+def getSchooling(name, age):
+    if (age < 18):
+        grade = int(input("What grade are you in " + name + "?"))
+        randNum2 = random.randint(0, 1)
+        if (randNum2 == 0):
+            print("Ugh. I hated grade" + grade + "!")
+        elif (randNum2 == 1):
+            print("Cool! I loved grade" + grade + "!")
+        else:
+            print("Bad random number. Stopping.")
+    else:
+        major = str(input("What grade are you in " + name + "?"))
+        if (str.lower(major) == "art"):
+            getRandomResponse("bad")
+        else:
+            getRandomResponse("good")
+    goodbye(name)
+
+def goodbye(name):
+    print("Well " + name + ", it has been nice talking to you.")
 
 def getRandomResponse(badOrGood):
     if (badOrGood == "bad"):
-        print(badResponses[random.randint(0, len(badResponses))])
+        print(badResponses[random.randint(0, len(badResponses) - 1)])
     elif (badOrGood == "good"):
-        print(goodResponses[random.randint(0, len(goodResponses))])
+        print(goodResponses[random.randint(0, len(goodResponses) - 1)])
     else:
         print("Bad input")
 
-randQuestions = [
-    getMood,
-    getJob,
-    getMarriage,
-    "Do you go to school?",
-]
+goodResponses = [ "Great!", "Good to hear.", "Groovy!", "Excellent.", "Cool!", "Cool.", "Epic.", ]
 
-goodResponses = [
-    "Great!",
-    "Good to hear.",
-]
+badResponses = [ "Sorry.", "Sorry to hear that.", "Uh oh.", "Awww. Sorry.", "Damn.", "Darn.", "Crap.", "Uh oh.", ]
 
-badResponses = [
-    "Sorry.",
-    "Sorry to hear that.",
-    "Uh oh.",
-    "Awww. Sorry."
-]
-
-def run():
+def start():
     print("Hello!")
-    running = True
-    while running:
-        
-
+    getName()
+    
+start()
