01135c8588b0d1d6d45c1b2ce901fe703272ff28
carson <carsin@users.noreply.github.com>
2017-09-29 11:22:26 -1000

    a4: init

---
+++ school/unit4/a4.py
@@ -0,0 +1,25 @@
+fname = input("What is your first name?")
+lname = input("What is your last name?")
+
+studentId = int(input("Student ID: "))
+
+currClass = "init"
+
+classes = []
+classNums = []
+
+while (currClass.lower() != "stop"):
+    currClass = input("Enter the next class, STOP to end.\t")
+    if (currClass.lower() != "stop"):
+        classNum = int(input("Enter the room number: "))
+        classes.append(currClass)
+        classNums.append(classNum)
+    else:
+        print("*********************************************")
+        print("*\t\t\t\t\t*")
+        print("* " + lname + ", " + fname + "\t \t ID: " + str(studentId) + " \t*")
+        print("*********************************************")
+        print("*\t\t\t\t\t*")
+        for x in range (0, len(classes)):
+            print("* Block " + str(x + 1) + ": " + classes[x] + "\t Room: " + str(classNums[x]) + "\t *") 
+        print("*********************************************")
