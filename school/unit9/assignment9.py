932291fcb4bd80b5625253fd0c899ac7133d112d
carson <carsin@users.noreply.github.com>
2017-11-22 12:46:43 -1000

    a9: finish

--- school/unit9/assignment9.py
+++ school/unit9/assignment9.py
@@ -16,7 +16,6 @@ Add an event
 Print all events
 Print events in a specific month
 '''
-
 eventMonths = []
 eventDays = []
 eventYears = []
@@ -32,6 +31,33 @@ def addEvent(month, day, year, name):
     
 def printAllEvents():
     print("******************** List of Events ********************")
-    for i in range(eventMonths.len)
+    for i in range(len(eventMonths)):
         print(str(eventNames[i]))
-        print(str("Date: " + months[eventMonths[i] - 1] + eventDays[i] + ", " + eventYears[i]))
+        print("Date: " + str(months[eventMonths[i] - 1]) + " " + str(eventDays[i]) + ", " + str(eventYears[i]))
+        
+def printEventsInMonth(monthNum):
+    print("********** Events in " + months[monthNum - 1] + "**********")
+    for i in range(len(eventMonths)):
+        if (eventMonths[i] == monthNum):
+            print(str(eventNames[i]))
+            print("Date: " + str(months[eventMonths[i] - 1]) + " " + str(eventDays[i]) + ", " + str(eventYears[i]))
+
+def start():
+    enterAnotherDate = None
+    while (enterAnotherDate != "NO"):
+        eventName = input("What is the event: ")
+        eventMonth = int(input("What is the month (number): "))
+        if (eventMonth > 12 or eventMonth < 1):
+            eventMonth = 1
+        eventDay = int(input("What is the date: "))
+        if (eventDay > 31 or eventDay < 1):
+            eventDay = 1
+        eventYear = int(input("What is the year: "))
+        addEvent(eventMonth, eventDay, eventYear, eventName)
+        enterAnotherDate = input("Do you want to enter another date? NO to stop.")
+    
+    printAllEvents()
+    monthToSee = int(input("What month would you like to see? (Enter the month number)"))
+    printEventsInMonth(monthToSee)
+    
+start()
